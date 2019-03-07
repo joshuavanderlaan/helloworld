867becd70e153c3dc634dea74899644582889f17
joshuavanderlaan <47641963+joshuavanderlaan@users.noreply.github.com>
2019-03-07 12:49:39 -0500

    Add files via upload

--- main.lua
+++ main.lua
@@ -1,26 +1,7 @@
-
 -----------------------------------------------------------------------------------------
-
 --
-
 -- main.lua
-
 --
-
--- Created by: Josh van der Laan
-
--- Created on: Feb 8, 2019
-
--- 
-
--- This file prints "Hello, World!" to the console
-
 -----------------------------------------------------------------------------------------
 
-
-
-display.setStatusBar(display.HiddenStatusBar)
-
-
-local rect = display.newRect( 0, 0, 100, 200 )
-print( "Display Object: " .. tostring( rect ) )
+-- Your code here
